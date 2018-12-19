ab18e39ed20b3d4b4527b7d6c712a70d83d724a2
Allen Hsieh <10136383+allenhsieh@users.noreply.github.com>
2018-12-18 19:20:09 -0800

    window.RoomApp no longer needed (#23)

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RoomApp from './components/App.jsx';
-window.RoomApp = RoomApp;
 
 ReactDOM.render(<RoomApp />, document.getElementById('Details'));
